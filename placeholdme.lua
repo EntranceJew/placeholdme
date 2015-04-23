46db0df9f55c1217088ad71ac21e9bb53f3dc06a
EntranceJew <EntranceJew@gmail.com>
2015-04-23 18:14:01 -0400

    Initial commit.

---
+++ placeholdme.lua
@@ -0,0 +1,67 @@
+-- a lib to generate placeholder images
+local placeholdme = {}
+
+-- from: http://lua-users.org/wiki/StringInterpolation
+local function interp(s, tab)
+	return (s:gsub('%%%((%a%w*)%)([-0-9%.]*[cdeEfgGiouxXsq])',
+			function(k, fmt) return tab[k] and ("%"..fmt):format(tab[k]) or
+				'%('..k..')'..fmt end))
+end
+-- refer to: https://docs.python.org/2/library/stdtypes.html#string-formatting if confused
+
+-- from: https://github.com/rxi/lume/#lumemerge
+local function tableMerge(t, t2, retainkeys)
+	for k, v in pairs(t2) do
+		t[retainkeys and k or (#t + 1)] = v
+	end
+	return t
+end
+
+local function newPlaceHolder(self, width, height, columns, rows, sprintstring, sprintvars, fontcolor, fillcolor)
+	columns = columns or 1
+	rows = rows or 1
+	sprintstring = sprintstring or "placeholder\n%(width)sx%(height)s \n%(column)s,%(row)s"
+	fontcolor = fontcolor or {255, 255, 255, 255}
+	fillcolor = fillcolor or {128, 128, 128, 255}
+	--[[
+		valid vars:
+			column	column number of frame
+			row		row number of frame
+			width	frame width
+			height	frame height
+	]]
+	local precolor = {love.graphics.getColor()}
+	local iData = love.image.newImageData(width*columns, height*rows)
+	local canvas = love.graphics.newCanvas(width, height)
+	love.graphics.setCanvas(canvas)
+	for h=1,rows do
+		for w=1,columns do
+			-- @TODO: Precalculate image/font height to center things horizontally.
+			-- fill
+			canvas:clear(fillcolor)
+			
+			-- do the text
+			love.graphics.setColor(fontcolor)
+			local providedVars = {column=w, row=h, width=width, height=height}
+			local str2sprint = interp(sprintstring, tableMerge(providedVars, sprintvars))
+			love.graphics.printf(str2sprint, 0, 0, width, "center")
+			
+			-- put the canvas on the master image
+			local canvasData = canvas:getImageData()
+			iData:paste(canvasData, width*(w-1), height*(h-1), 0, 0, width, height)
+		end
+	end
+	
+	-- reset the graphical state
+	love.graphics.setCanvas()
+	love.graphics.setColor(precolor)
+	return iData
+end
+
+local metaTable = {
+	__call = newPlaceHolder
+}
+
+setmetatable(placeholdme, metaTable)
+
+return placeholdme
---
+++ readme.md
@@ -0,0 +1,35 @@
+# placeholdme
+A love lib for generating a temporary sprite sheet.
+
+Use it anywhere you would usually reference an asset. You don't need to wait to have visual assets to start spinning out some code.
+
+# Parameters
+* `width` The width of a single frame of the spritesheet.
+* `height` The height of a single frame of the spritesheet.
+* `columns` The number of horizontal frames the spirtesheet should have.
+* `rows` The number of vertical frames the spritesheet should have.
+* `sprintstring` A formatted string with named parameters. [See: Named Parameters with Formatting Codes](http://lua-users.org/wiki/StringInterpolation) for more information. Provided parameters are:
+	* `column` The current horizontal column number of the spritesheet that the image is in.
+	* `row` The current vertical row number of the spritesheet that the image is in.
+	* `width` The width of the individual frame of the spritesheet.
+	* `height` The height of the individual frame of the spritesheet.
+* `sprintvars` A table of named elements that will be merged with the above provided vars. This can be used to insert custom elements into the sprintstring.
+* `fontcolor` The color of the font to use when drawing the text.
+* `fillcolor` The color to make the placeholder background.
+
+# Example
+```lua
+-- drop it in your project
+placeholdme = require 'placeholdme'
+
+function love.load()
+	local imageSource = placeholdme(100,50,3,4)
+	-- instead of 'image/path/here.png', just put placeholdme
+	placeholder = love.graphics.newImage(imageSource)
+end
+
+function love.draw()
+	-- d
+	love.graphics.draw(placeholder, 0, 0)
+end
+```
